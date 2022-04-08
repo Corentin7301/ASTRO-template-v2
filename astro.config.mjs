2fae5e1eb6712575c2e7e4d16f38076e43badda8
Corentin PERROUX <corentin7301@gmail.com>
2022-04-08 19:54:56 +0200

    add Astro sitemap configuration add Astro turbolink integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
-import { defineConfig } from 'astro/config';
+import {
+  defineConfig
+} from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import vue from '@astrojs/vue';
 import sitemap from '@astrojs/sitemap';
+import turbolinks from "@astrojs/turbolinks";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [tailwind(),vue(),sitemap()],
+  site: 'https://sitename.fr',
+  integrations: [tailwind(), vue(), sitemap(), turbolinks()]
 });
